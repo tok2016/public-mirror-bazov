e4479b8567edc1366948b0b62cb4839ef399574a
tok2016 <ratchet23122003@gmail.com>
2026-06-25 12:14:03 +0500

    fixes meta controllers hints

--- Assets/Main/Scripts/Player/Controllers/MetaControllerState/MetaButtonPressState.cs
+++ Assets/Main/Scripts/Player/Controllers/MetaControllerState/MetaButtonPressState.cs
@@ -9,11 +9,14 @@ public class MetaButtonPressState : MetaState
     {
         if (_stateMachine.IsDisabled)
             _stateMachine.ChangeState(_stateMachine.DisabledState);
-        else if (_stateMachine.IsWarned)
-            _stateMachine.ChangeState(_stateMachine.WarnState);
         else if (_stateMachine.IsPaused)
             _stateMachine.ChangeState(_stateMachine.PauseState);
         else if (!_stateMachine.IsPressed)
-            _stateMachine.ChangeState(_stateMachine.DefaultState);
+        {
+            if (_stateMachine.IsWarned)
+                _stateMachine.ChangeState(_stateMachine.WarnState);
+            else
+                _stateMachine.ChangeState(_stateMachine.DefaultState);
+        }
     }
 }
